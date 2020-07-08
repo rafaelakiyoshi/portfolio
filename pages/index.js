f24890f9fc3e67dc5e0e14361445491a68fc5191
Rafael Akiyoshi <rafah94@hotmail.com>
2020-07-07 21:54:01 -0400

    updating root page and yarn lock

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import Head from "next/head";
+import MyParticles from "../components/particles";
+import About from "../components/about"
 
 export default function Home() {
   return (
@@ -8,21 +8,15 @@ export default function Home() {
       <Head>
         <title>Rafael Akiyoshi</title>
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Cutive+Mono&display=swap" rel="stylesheet"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cutive+Mono&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
-      <div>
-        <h1 className="kanji">秋吉</h1>
-        <h1 className="my-name">Rafael Akiyoshi</h1>
-        <p className="my-desc">Software Engineer</p>
-        <div className="icons-list">
-          <a href="https://github.com/rafaelakiyoshi">
-            <GitHubIcon  style={{ fontSize: 20 }} className="logo"/>
-          </a>
-          <a href="https://www.linkedin.com/in/rafaelakiyoshi/">
-            <LinkedInIcon  style={{ fontSize: 25 }} className="logo"/>
-          </a>
-        </div>
+      <MyParticles />
+      <div className="main-container">
+        <About />
       </div>
     </div>
-  )
+  );
 }
