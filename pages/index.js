3b9b96660111ab3def63ae738f568f911b8251c7
Rafael Akiyoshi <rafah94@hotmail.com>
2020-07-07 20:01:22 -0400

    portfolio initial design

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,17 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="main-container">
       <Head>
         <title>Rafael Akiyoshi</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Cutive+Mono&display=swap" rel="stylesheet"></link>
       </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome
-        </h1>
-      </main>
+      <div>
+        <h1 className="kanji">秋吉</h1>
+        <h1 className="my-name">Rafael Akiyoshi</h1>
+        <p className="my-desc">Software Engineer</p>
+      </div>
     </div>
   )
 }
