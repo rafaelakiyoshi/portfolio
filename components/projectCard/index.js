5500d9585d6d42e3fdc63270b32ad75b9587a61a
Rafael Akiyoshi <rafah94@hotmail.com>
2020-08-02 15:51:38 -0400

    implementing animation in projectCard component

--- components/projectCard/index.js
+++ components/projectCard/index.js
@@ -1,21 +1,28 @@
+import { motion } from "framer-motion";
 import styles from "./projectCard.module.css";
+
 const ProjectCard = (props) => {
-  const { title, description, technologies, setProjectUrl } = props;
+  const { title, description, technologies, setProjectUrl, projectUrl } = props;
   return (
-    <div className={styles.project} onClick={() => setProjectUrl("https://www.google.com")}>
-      <div className={styles.github}>
-        <img src="./github.svg" className={styles.githubIcon} />
+    <motion.div
+      whileHover={{ scale: 1.05, transition: { duration: 0.05 } }}
+      whileTap={{ scale: 0.95, transition: { duration: 0.05 } }}
+    >
+      <div className={styles.project} onClick={() => setProjectUrl(projectUrl)}>
+        <div className={styles.github}>
+          <img src="./github.svg" className={styles.githubIcon} />
+        </div>
+        <h1 className="kanji">{title}</h1>
+        <p>{description}</p>
+        <div className={styles.technologies}>
+          {technologies.map((technology) => {
+            return (
+              <img src={`./${technology}.svg`} className={styles.technology} />
+            );
+          })}
+        </div>
       </div>
-      <h1 className="kanji">{title}</h1>
-      <p>{description}</p>
-      <div className={styles.technologies}>
-        {technologies.map(technology => {
-          return (
-            <img src={`./${technology}.svg`} className={styles.technology} />
-          )
-        })}
-      </div>
-    </div>
+    </motion.div>
   );
 };
 
