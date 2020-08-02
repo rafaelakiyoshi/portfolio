90ab6d92cd62371aaf0b14d3c753a120ef6d32d3
Rafael Akiyoshi <rafah94@hotmail.com>
2020-08-02 15:52:49 -0400

    improving about component with animation

--- components/about/index.js
+++ components/about/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import styles from './about.module.css';
+import { motion } from "framer-motion";
+import styles from "./about.module.css";
 
 const About = () => {
   return (
     <div className={styles.aboutContainer}>
-      <h1 className={styles.kanji}>秋吉</h1>
+      <motion.div
+        animate={{ y: -10 }}
+        transition={{ duration: 1, yoyo: Infinity }}
+      >
+        <h1 className={styles.kanji}>秋吉</h1>
+      </motion.div>
       <h1 className={styles.myName}>Rafael Akiyoshi</h1>
       <p className={styles.myDesc}>Software Engineer</p>
       <div className={styles.iconsList}>
